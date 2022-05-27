39a58c62d044706528e0ffd5186a1c9c15d9142e
tzuyu <s1080310@gm.pu.edu.tw>
2022-05-27 22:51:31 +0800

    gamed完成

--- app/src/main/java/tw/edu/pu/s1080310/graduationtopic/gameActivity3_4.kt
+++ app/src/main/java/tw/edu/pu/s1080310/graduationtopic/gameActivity3_4.kt
@@ -1,13 +1,16 @@
 package tw.edu.pu.s1080310.graduationtopic
 
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class gameActivity3_4 : AppCompatActivity() {
 
-    lateinit var mper: MediaPlayer
+    private var soundPool1: SoundPool? = null
+    private val soundId = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +19,23 @@ class gameActivity3_4 : AppCompatActivity() {
         getSupportActionBar()?.hide();
 
 
+        soundPool1 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+        soundPool1!!.load(baseContext, R.raw.amsound, 0)
+
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+
         val img17: ImageView = findViewById(R.id.img17)
         GlideApp.with(this)
             .load(R.drawable.game341)
             .into(img17)
 
-        mper = MediaPlayer.create(this, R.raw.amsound)
-        mper.start()
+        soundPool1?.play(soundId, 1F, 1F, 0, 3, 1F)
+
 
     }
 }
